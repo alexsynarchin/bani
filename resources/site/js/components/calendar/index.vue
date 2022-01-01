cee6b85a30cf19c25c1b8c07b04db82c9531442a
Алексей <alexsynarchin@gmail.com>
2022-01-01 21:25:04 +0500

    reserved places

--- resources/site/js/components/calendar/index.vue
+++ resources/site/js/components/calendar/index.vue
@@ -86,7 +86,7 @@ export default {
 
         today() {
             let day = dayjs().format("YYYY-MM-DD")
-            return '2022-01-01';
+            return '2022-01-02';
         },
 
         month() {
