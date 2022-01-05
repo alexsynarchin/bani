dd40fbe263cfa7c5fd354a3b39ba339ee35035ef
Алексей <alexsynarchin@gmail.com>
2022-01-05 18:57:40 +0500

    data change today

--- resources/site/js/components/calendar/index.vue
+++ resources/site/js/components/calendar/index.vue
@@ -86,7 +86,7 @@ export default {
 
         today() {
             let day = dayjs().format("YYYY-MM-DD")
-            return '2022-01-05';
+            return '2022-01-06';
         },
 
         month() {
