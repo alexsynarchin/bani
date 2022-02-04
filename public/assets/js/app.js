d02c0ed282f1e2acb47b5fcc5685f81591a634a9
Алексей <alexsynarchin@gmail.com>
2022-02-04 16:04:10 +0500

    today

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -4657,7 +4657,7 @@ dayjs__WEBPACK_IMPORTED_MODULE_0___default().extend((dayjs_plugin_weekOfYear__WE
       var now = new Date();
 
       if (now.getHours() >= 16) {
-        day = '2022-02-04';
+        day = '2022-02-05';
       }
 
       return day;
