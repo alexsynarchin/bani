4304c9d6b9cefdcce0060ccb7d326d4f8515987b
Алексей <alexsynarchin@gmail.com>
2022-05-25 10:34:33 +0500

    Fix richtext bug in admin

--- app/Http/Controllers/Api/PlaceController.php
+++ app/Http/Controllers/Api/PlaceController.php
@@ -10,13 +10,14 @@ use Illuminate\Http\Request;
 class PlaceController extends Controller
 {
     public function index(Request $request, $floor) {
+
         $places = Place::where('floor', $floor) -> get();
         foreach ($places as $place) {
             $place -> reserved = false;
            $reservations = $place ->reservations()
                ->whereDate('date', '=', $request->get('date'))
-               ->whereTime('start', '<=', \Carbon\Carbon::parse($request->get('startDate')))
-               -> whereTime('end' , '>', \Carbon\Carbon::parse($request->get('startDate'))->addMinutes(30))
+               ->whereTime('end', '>=', \Carbon\Carbon::parse($request->get('startDate')))
+               -> whereTime('start' , '<=', \Carbon\Carbon::parse($request->get('endDate')))
                ->whereHas('order', function ($query) {
                $query->where('status','success');
            }) -> get();
