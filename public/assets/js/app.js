4e498bb6007007301583c70c4b85231cf57f0330
Алексей <alexsynarchin@gmail.com>
2022-06-09 16:32:03 +0500

    new domen

--- public/assets/js/app.js
+++ public/assets/js/app.js
@@ -4690,7 +4690,7 @@ dayjs__WEBPACK_IMPORTED_MODULE_0___default().extend((dayjs_plugin_weekOfYear__WE
       var now = new Date();
 
       if (now.getHours() >= 16) {
-        day = '2022-06-09';
+        day = '2022-06-10';
       }
 
       return day;
