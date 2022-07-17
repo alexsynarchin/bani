a14bbf5cb88a2252226f8a48dbcff2dbef1da764
Алексей <alexsynarchin@gmail.com>
2022-07-17 13:01:42 +0500

    new domen

--- resources/site/js/components/calendar/index.vue
+++ resources/site/js/components/calendar/index.vue
@@ -106,7 +106,7 @@ export default {
             let day = dayjs().format("YYYY-MM-DD");
             var now = new Date();
             if(now.getHours() >= 12) {
-                day = '2022-07-17'
+                day = '2022-07-18'
             }
             return day;
         },
